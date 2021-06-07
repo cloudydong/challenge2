56513c84b783f286db3d2ed93bf8c49221e6d40c
cloudydong <cloudy12028@gmail.com>
2021-06-07 21:36:55 +0900

    podcast-list.spec

--- src/page/__test__/podcast/podcast-list.spec.tsx
+++ src/page/__test__/podcast/podcast-list.spec.tsx
@@ -1,49 +1,72 @@
-import { MockedProvider } from "@apollo/client/testing";
-import { render, waitFor } from "@testing-library/react";
+import { ApolloProvider } from "@apollo/client";
+import { render, RenderResult, waitFor } from "@testing-library/react";
+import { createMockClient, MockApolloClient } from "mock-apollo-client";
 import React from "react";
 import { HelmetProvider } from "react-helmet-async";
 import { BrowserRouter as Router } from "react-router-dom";
 import { PodcastList, PODCAST_LIST_QUERY } from "../../podcast/podcast-list";
 
+export const mockedPodcastListQueryResponse = Promise.resolve({
+  data: {
+    getAllPodcasts: {
+      __typename: "Podcasts",
+      podcasts: [
+        {
+          __typename: "Podcast",
+          id: 1,
+          updateAt: "",
+          title: "test",
+          category: "test",
+          rating: 5,
+        },
+      ],
+    },
+  },
+});
+
+const mockPush = jest.fn();
+jest.mock("react-router-dom", () => {
+  const realModule = jest.requireActual("react-router-dom");
+  return {
+    ...realModule,
+    useHistory: () => {
+      return { push: mockPush };
+    },
+  };
+});
+
 describe("<PodcastList />", () => {
-  it("how to mocked PODCAST_LIST_QUERY", async () => {
-    await waitFor(async () => {
-      render(
-        <HelmetProvider>
-          <MockedProvider
-            mocks={[
-              {
-                request: {
-                  query: PODCAST_LIST_QUERY,
-                },
-                result: {
-                  data: {
-                    getAllPodcasts: {
-                      podcasts: {
-                        id: 1,
-                        updateAt: "",
-                        title: "test",
-                        category: "test",
-                        rating: 5,
-                      },
-                    },
-                  },
-                },
-              },
-            ]}
-          >
+  let mockedClient: MockApolloClient;
+  let renderResult: RenderResult;
+  beforeEach(async () => {
+    await waitFor(() => {
+      mockedClient = createMockClient();
+      // mockedClient.setRequestHandler(
+      //   PODCAST_LIST_QUERY,
+      //   () => mockedPodcastListQueryResponse
+      // );
+      const queryHandler = jest
+        .fn()
+        .mockResolvedValue(mockedPodcastListQueryResponse);
+      mockedClient.setRequestHandler(PODCAST_LIST_QUERY, queryHandler);
+      renderResult = render(
+        <ApolloProvider client={mockedClient}>
+          <HelmetProvider>
             <Router>
               <PodcastList />
             </Router>
-          </MockedProvider>
-        </HelmetProvider>
+          </HelmetProvider>
+        </ApolloProvider>
       );
-
-      await new Promise((resolve) => setTimeout(resolve, 0));
-      // debug();
-      await waitFor(() => {
-        expect(document.title).toBe("Podcast List | Challenge");
-      });
     });
+  });
+
+  afterAll(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should render OK with props", async () => {
+    const { getByText } = renderResult;
+    getByText("test");
   });
 });
