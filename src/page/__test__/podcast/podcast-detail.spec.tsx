01074d699e79cc015a7ed2e357d67556024aad0e
cloudydong <cloudy12028@gmail.com>
2021-06-08 01:12:56 +0900

    podcast-detail spec

--- src/page/__test__/podcast/podcast-detail.spec.tsx
+++ src/page/__test__/podcast/podcast-detail.spec.tsx
@@ -1,50 +1,71 @@
-import { MockedProvider } from "@apollo/client/testing";
-import { render, waitFor } from "@testing-library/react";
+import { ApolloProvider } from "@apollo/client";
+import { render, RenderResult, waitFor } from "@testing-library/react";
+import { createMockClient, MockApolloClient } from "mock-apollo-client";
 import React from "react";
 import { HelmetProvider } from "react-helmet-async";
 import { BrowserRouter as Router } from "react-router-dom";
 import { GET_PODCAST_QUERY, PodcastDetail } from "../../podcast/podcast-detail";
 
+const longStr161 = Array(162).join("x");
+const mockedPodcastDetailQueryResponse = {
+  data: {
+    getPodcast: {
+      __typename: "getPodcast",
+      podcast: {
+        __typename: "Podcast",
+        id: 1,
+        title: "test",
+        category: "test",
+        episodes: [
+          {
+            __typename: "Episodes",
+            id: 1,
+            title: "test",
+            category: longStr161,
+          },
+          {
+            __typename: "Episodes",
+            id: 2,
+            title: "test",
+            category: "test",
+          },
+        ],
+      },
+    },
+  },
+};
 describe("<PodcastDetail />", () => {
-  it("how to mocked Graphql Query", async () => {
-    await waitFor(async () => {
-      render(
-        <HelmetProvider>
-          <MockedProvider
-            mocks={[
-              {
-                request: {
-                  query: GET_PODCAST_QUERY,
-                },
-                result: {
-                  data: {
-                    podcast: {
-                      id: 1,
-                      title: "test",
-                      category: "test",
-                      episodes: {
-                        id: 1,
-                        title: "test",
-                        category: "test",
-                      },
-                    },
-                  },
-                },
-              },
-            ]}
-          >
+  let mockedClient: MockApolloClient;
+  let renderResult: RenderResult;
+  beforeEach(async () => {
+    await waitFor(() => {
+      mockedClient = createMockClient();
+      const queryHandler = jest
+        .fn()
+        .mockResolvedValue(mockedPodcastDetailQueryResponse);
+      mockedClient.setRequestHandler(GET_PODCAST_QUERY, queryHandler);
+      renderResult = render(
+        <ApolloProvider client={mockedClient}>
+          <HelmetProvider>
             <Router>
               <PodcastDetail />
             </Router>
-          </MockedProvider>
-        </HelmetProvider>
+          </HelmetProvider>
+        </ApolloProvider>
       );
-
-      await new Promise((resolve) => setTimeout(resolve, 0));
-      // debug();
-      await waitFor(() => {
-        expect(document.title).toBe("Podcast Detail | Challenge");
-      });
     });
+  });
+
+  afterAll(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should render OK with props", async () => {
+    const { queryAllByText, getByText } = renderResult;
+    queryAllByText("test");
+    await waitFor(() => {
+      expect(document.title).toBe("Podcast Detail | Challenge");
+    });
+    getByText(longStr161.substring(0, 160) + "...");
   });
 });
